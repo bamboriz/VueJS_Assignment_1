f37a14bb4fffd849118920d450556b626eda5c66
Boris B <bamboriz1@gmail.com>
2018-08-18 08:48:08 +0100

    content code

---
+++ app.js
@@ -0,0 +1,23 @@
+Vue.component('name', {
+  props: ['school'],
+  template: '<li>{{ school.student }}</li>'
+});
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    studentList: [
+      { id: 0, student: 'James' },
+      { id: 1, student: 'John' },
+      { id: 2, student: 'Jean' },
+      { id: 3, student: 'Mary' },
+      { id: 4, student: 'Lyn' },
+      { id: 5, student: 'Kayla' },
+      { id: 6, student: 'Puth' },
+      { id: 7, student: 'Selena' },
+      { id: 8, student: 'Greg' },
+      { id: 9, student: 'Boris' },
+      { id: 10, student: 'Brenda' },
+    ]
+  }
+})
